319e57d638ce747b5585a735199e3183e6737043
FIRE Araştırma Eğitim Ltd. Şti <162897064+fire-ae@users.noreply.github.com>
2025-04-22 22:43:47 +0300

    Update litrevmain.py

--- NASA-ADS/litrevmain.py
+++ NASA-ADS/litrevmain.py
@@ -1,5 +1,5 @@
 import ads
-from litrevlib import query_ads, print_lit, handle_selection
+from litrevlib import query_ads, abstract_ads, print_lit, handle_selection
 
 ads.config.token = '' # You should generate this token after registering NASA/ADS freely. You can do this via Accounts >> Settings >> Account Settings >> API Token
 
@@ -7,7 +7,8 @@ ads.config.token = '' # You should generate this token after registering NASA/AD
 
 query = ['lunar thermal environment'] # specify your query, like what you would enter when you reach out NASA/ADS web api search box.
 
-results = query_ads(query,1) # 1 page = 50 entries at max.
+results = query_ads(query,1) # 1 page = 50 entries at max. "query" is a list of strings
+results2 = abstract_ads(query,1) # queries the abstract field of NASA ADS entries, takes a string, not a list of strings
 
 locat = "" #location for your .bib file to transfer bibtex entries
 
